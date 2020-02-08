b849f6a115ce8b3666fd525bfeaac784cd1cb183
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-02-08 13:26:20 +0530

    Remove Board component

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import Board from './Board';
+import './index.css';
 
-class Game extends React.Component {
+function Square(props) {
+	return (
+		<button className='square' onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
+}
+class TicTacToe extends React.Component {
+	renderSquare(i) {
+		return (
+			<Square
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(this.props.id, i)}
+			/>
+		);
+	}
+
 	render() {
+		const rows = 3;
+		const columns = 3;
 		return (
 			<div className='game-board'>
-				<Board
-					squares={this.props.squares}
-					onClick={i => this.props.onClick(this.props.id, i)}
-				/>
+				{[...new Array(rows)].map((x, rowIndex) => {
+					return (
+						<div className='board-row'>
+							{[...new Array(columns)].map((y, colIndex) =>
+								this.renderSquare(rowIndex * columns + colIndex)
+							)}
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
 }
 
-export default Game;
+export default TicTacToe;
