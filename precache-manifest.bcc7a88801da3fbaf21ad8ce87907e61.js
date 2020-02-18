0aabdf1d07fe129e48c6007936330701da3d8d6f
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-02-18 17:51:34 +0530

    Updates

---
+++ precache-manifest.bcc7a88801da3fbaf21ad8ce87907e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96a46de41d5004c6f55102934abfb4d",
+    "url": "/tttxttt/index.html"
+  },
+  {
+    "revision": "df5e116b35be548b4d58",
+    "url": "/tttxttt/static/css/main.46563674.chunk.css"
+  },
+  {
+    "revision": "c381e3a453d0c8e42db1",
+    "url": "/tttxttt/static/js/2.27554377.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/tttxttt/static/js/2.27554377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5e116b35be548b4d58",
+    "url": "/tttxttt/static/js/main.6ae5f72d.chunk.js"
+  },
+  {
+    "revision": "ba56865020e9b9e80727",
+    "url": "/tttxttt/static/js/runtime-main.2d4013d6.js"
+  }
+]);
